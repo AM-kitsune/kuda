07dadcca8b4d8f4ad6699e72efc919880462243e
Am-Kitsune <105692663+AM-kitsune@users.noreply.github.com>
2024-09-04 11:07:25 +0700

    biodata

---
+++ Tugas14.java
@@ -0,0 +1,15 @@
+public class Tugas14 {
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------------------");
+        System.out.println("--  Data Mahasiswa Jurusan Teknologi Informasi  --");
+        System.out.println("--------------------------------------------------");
+        System.out.println("Nama     : Khesa Kresna Adjieningrat");
+        System.out.println("NIM      : 244107020233");
+        System.out.println("Kelas    : 1B");
+        System.out.println("No Absen : 14");
+        System.out.println("Prodi    : D-IV Teknik Informatika");
+        System.out.println("Alamat   : Jl. Soekarno Hatta Bendo, Pare, Kediri");
+        System.out.println("No Hp    : 081217812662");
+        System.out.println("Hobi     : Bermain Game");
+    }
+}
